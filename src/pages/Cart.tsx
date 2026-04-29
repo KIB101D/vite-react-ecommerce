004e77e441aee7bf288e0ba142f56912b678ab5a
KIB101D <152413382+KIB101D@users.noreply.github.com>
2026-04-30 00:49:13 +0200

    feat(cart): implement interactive cart UI with quantity controls and item navigation
    - add quantity controls
    - add item removal
    - add navigation to cart items

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,16 @@
-function Cart() {
+import { useCart } from "../hooks/useCart";
+import deleteIcon from "../assets/delete-bin.svg";
+import { Link } from "react-router-dom";
+
+function Cart({ products }) {
+  const { cart, removeFromCart, increment, decrement } = useCart();
+  const subtotal = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0,
+  );
+  const shipping = subtotal > 100 ? 0 : 10;
+  const total = subtotal + shipping;
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto">
       <h1 className="mb-10 text-3xl font-semibold text-gray-800 font-heading">
@@ -10,72 +22,80 @@ function Cart() {
         <div className="flex justify-center lg:justify-start">
           <div className="w-full max-w-2xl space-y-6">
             {/* Item */}
-            <div className="p-5 transition bg-white border rounded-xl hover:shadow-lg">
-              <div className="flex gap-4">
-                <img
-                  src="https://placehold.co/100/png"
-                  className="object-cover w-20 h-20 bg-gray-100 rounded-lg shrink-0"
-                />
+            {cart.map((product) => {
+              return (
+                <div
+                  key={product.id}
+                  className="relative p-5 transition bg-white border rounded-xl hover:shadow-lg"
+                >
+                  {/* 🗑 ICON */}
+                  <button
+                    onClick={() => removeFromCart(product.id)}
+                    className="absolute flex items-center justify-center text-gray-400 transition border border-gray-300 rounded-md top-3 right-3 w-7 h-7 hover:text-red-500 hover:border-red-300 hover:bg-red-50"
+                  >
+                    <img
+                      src={deleteIcon}
+                      alt="delete"
+                      className="w-4 h-4 opacity-70 hover:opacity-100"
+                    />
+                  </button>
+                  <div className="flex gap-4">
+                    <Link
+                      to={`/category/${product.categoryId}/product/${product.id}`}
+                    >
+                      <img
+                        src={product.image}
+                        className="object-cover w-20 h-20 transition bg-gray-100 rounded-lg shrink-0 hover:opacity-80"
+                      />
+                    </Link>
 
-                <div className="flex flex-col justify-between flex-1">
-                  {/* title */}
-                  <div>
-                    <p className="font-medium text-gray-700">Product Name</p>
-                    <p className="text-sm text-gray-500">Category</p>
-                  </div>
+                    <div className="flex flex-col justify-between flex-1">
+                      {/* title */}
+                      <div className="flex flex-col gap-1">
+                        <Link
+                          to={`/category/${product.categoryId}/product/${product.id}`}
+                          className="font-medium text-gray-800 transition w-fit hover:text-indigo-600"
+                        >
+                          {product.title}
+                        </Link>
 
-                  {/* controls */}
-                  <div className="flex items-center justify-between mt-4">
-                    <div className="flex items-center gap-3">
-                      <button className="flex items-center justify-center text-white transition bg-gray-800 rounded-lg w-7 h-7 hover:bg-gray-700">
-                        −
-                      </button>
+                        <Link
+                          to={`/category/${product.categoryId}`}
+                          className="inline-block px-2 py-1 text-xs text-gray-600 bg-gray-100 rounded-md w-fit hover:bg-gray-200"
+                        >
+                          {product.categoryId}
+                        </Link>
+                      </div>
 
-                      <span className="text-sm font-medium">1</span>
+                      {/* controls */}
+                      <div className="flex items-center justify-between mt-4">
+                        <div className="flex items-center gap-3">
+                          <button
+                            onClick={() => decrement(product.id)}
+                            className="flex items-center justify-center text-white transition bg-gray-800 rounded-lg w-7 h-7 hover:bg-gray-700"
+                          >
+                            −
+                          </button>
 
-                      <button className="flex items-center justify-center text-white transition bg-gray-800 rounded-lg w-7 h-7 hover:bg-gray-700">
-                        +
-                      </button>
+                          <span className="text-sm font-medium">
+                            {product.quantity}
+                          </span>
+
+                          <button
+                            onClick={() => increment(product.id)}
+                            className="flex items-center justify-center text-white transition bg-gray-800 rounded-lg w-7 h-7 hover:bg-gray-700"
+                          >
+                            +
+                          </button>
+                        </div>
+
+                        <p className="font-semibold ">{product.price}$</p>
+                      </div>
                     </div>
-
-                    <p className="font-semibold ">$99</p>
                   </div>
                 </div>
-              </div>
-            </div>
-
-            {/* Item */}
-            <div className="p-5 transition bg-white border rounded-xl hover:shadow-lg">
-              <div className="flex gap-4">
-                <img
-                  src="https://placehold.co/100/png"
-                  className="object-cover w-20 h-20 bg-gray-100 rounded-lg shrink-0"
-                />
-
-                <div className="flex flex-col justify-between flex-1">
-                  <div>
-                    <p className="font-medium text-gray-700">Another Product</p>
-                    <p className="text-sm text-gray-500">Category</p>
-                  </div>
-
-                  <div className="flex items-center justify-between mt-4">
-                    <div className="flex items-center gap-3">
-                      <button className="flex items-center justify-center text-white transition bg-gray-800 rounded-lg w-7 h-7 hover:bg-gray-700">
-                        −
-                      </button>
-
-                      <span className="text-sm font-medium">2</span>
-
-                      <button className="flex items-center justify-center text-white transition bg-gray-800 rounded-lg w-7 h-7 hover:bg-gray-700">
-                        +
-                      </button>
-                    </div>
-
-                    <p className="font-semibold">$198</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
 
@@ -86,17 +106,17 @@ function Cart() {
           <div className="space-y-3 text-sm text-gray-600">
             <div className="flex justify-between">
               <span>Subtotal</span>
-              <span>$297</span>
+              <span>${subtotal}</span>
             </div>
 
             <div className="flex justify-between">
               <span>Shipping</span>
-              <span>$10</span>
+              <span>${shipping}</span>
             </div>
 
             <div className="flex justify-between pt-3 mt-3 text-base font-semibold text-gray-900 border-t">
               <span>Total</span>
-              <span>$307</span>
+              <span>${total}</span>
             </div>
           </div>
 
