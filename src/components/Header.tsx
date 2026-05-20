8a4c455e2543157b133387dceac86476f598cf33
KIB101D <152413382+KIB101D@users.noreply.github.com>
2026-05-20 14:02:29 +0200

    chore: remove unused useLocation import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import logo from "../assets/react-blue.png";
 import SearchBar from "./SearchBar";
